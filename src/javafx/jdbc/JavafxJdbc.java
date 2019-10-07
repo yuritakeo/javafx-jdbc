b46b6d4de475985861dcb7b92bde7caad3ce0ade
yuritakeo <37599776+yuritakeo@users.noreply.github.com>
2019-10-07 18:49:03 -0300

    Main view desing

--- src/javafx/jdbc/JavafxJdbc.java
+++ src/javafx/jdbc/JavafxJdbc.java
@@ -7,8 +7,8 @@ package javafx.jdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,8 +21,12 @@ public class JavafxJdbc extends Application {
     public void start(Stage primaryStage) throws Exception {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-            Parent parent = loader.load();
-            Scene mainScene = new Scene(parent);
+            ScrollPane scrollPane = loader.load();
+            
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+            
+            Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
