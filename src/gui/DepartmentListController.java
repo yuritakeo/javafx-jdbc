c75b49566950495d6b289dc2950aa1c7ab07884c
yuritakeo <37599776+yuritakeo@users.noreply.github.com>
2019-10-11 16:54:15 -0300

    DepartmentList Controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.jdbc.Main;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+/**
+ *
+ * @author yuri.miyazaki
+ */
+public class DepartmentListController implements Initializable {
+
+    @FXML
+    private TableView<Department> tableViewDepartment;
+
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private Button btNew;
+
+    @FXML
+    public void onBtNew_Action() {
+        System.out.println("onBtNew_Action");
+    }
+
+    @Override
+    public void initialize(URL uri, ResourceBundle rb) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
+}
